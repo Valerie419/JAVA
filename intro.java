0d46c655640d102b5723e28757323be4bbee2fb8
Valerie <oguziesochimazuru@gmail.com>
2025-08-08 10:15:39 +0100

    Folder for learning java

---
+++ intro.java
@@ -0,0 +1,13 @@
+public class intro{  
+    public static void main(String[] args){
+
+        
+    }
+
+}
+
+
+
+    
+// public- access modifier, means the class can be accessed anywhere
+// class- 
